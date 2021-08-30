41bff6e6160f89a12a34651039253acc02a0828c
Artem Denysov <denysov.artem@gmail.com>
2021-08-30 11:38:43 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ async function run() {
     try {
         const filter = core.getInput('filter')
             .split("\n")
-	        .filter(x => x !== "");;
-        const filteredLabels = github.context.payload.pull_request.labels.map(({ name }) => {
-            return filter.includes(name);
-        });
+	    .filter(x => x !== "");;
+        const filteredLabels = github.context.payload.pull_request.labels.filter(({ name }) => filter.includes(name));
         core.setOutput('filteredLabels', filteredLabels);
     } catch (error) {
         core.error(error);
